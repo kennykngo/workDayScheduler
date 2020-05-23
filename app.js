6452a08b787eb301b48b64690353c33980cbde8a
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-05-23 01:09:06 -0700

    added features including a table to add tasks

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 $(document).ready(function () {
-  var time = moment().format("dddd, MMMM Do YYYY");
-  console.log(time);
+  var date = moment().format("dddd, MMMM Do YYYY");
 
   var currentDay = $("#currentDay");
-  currentDay.text(time);
+  currentDay.text(date);
+  // var timeBlocks = $("#timeBlocks");
+
+  var Date = moment().format("h");
+  console.log(parseInt(Date));
+  // $("#timeBlocks").text(Date.add("hours", 1));
+  // for (var i = 0; i < 8; i++) {
+  //   var current = moment().add(1, "hours").calendar;
+  //   console.log(current);
+  //   // console.log("hi");
+  // }
+
+  $("button").on("click", function () {
+    var dataId = $(this).attr("id");
+    var id = dataId.split("-")[0];
+    // passes a string number
+    var eleData = $("#" + id + "-input").val();
+    // console.log(eleData);
+    localStorage.setItem(id, eleData);
+
+    console.log(id);
+  });
+
+  var currentTime = moment().hours();
+
+  for (i = 9; i <= 17; i++) {
+    $(`#${i}-input`).val(localStorage.getItem(i));
+    console.log("hello");
+    if (i < currentTime) {
+      console.log("hello world");
+      $(`#${i}-input`).addClass("past");
+    } else if (i === currentTime) {
+      $(`#${i}-input`).addClass("present");
+    } else {
+      $(`#${i}-input`).addClass("future");
+    }
+  }
+
+  console.log(currentTime);
+
   console.log(currentDay);
 });
